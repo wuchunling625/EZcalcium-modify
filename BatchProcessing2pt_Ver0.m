530995b20fa628fa41ed261cb9636020f3d5b91d
lark <770644125@qq.com>
2021-12-26 23:01:15 +0800

    commit code

---
+++ BatchProcessing2pt_Ver0.m
@@ -0,0 +1,119 @@
+﻿%% 2ptBatchProcessing_Ver0
+%   =======================================================================================
+%   =======================================================================================
+clear,clc;
+mainPath = 'C:\Users\linji\Desktop\ljh'; % scan target folder
+hasCh2 =true;       % whether has channel 2
+
+TargetLabel = 'ch2'; % the target images folder
+ch2Lable = 'follow'; % channel 2 images folder
+
+TargetType = 1; % only support folder
+filetype = 'tif';% the target image type
+%% 
+cd(mainPath);
+TargetList = scanDir(mainPath,TargetLabel,TargetType);
+TargetList = TargetList';
+save TargetList;
+len = size(TargetList,1);
+for i = 1:len
+    TargetFolder = TargetList{i};
+    disp(['Exporting ',TargetFolder]);
+    cd(TargetFolder);
+    targetPack = dir(['*.',filetype]);
+    filesNO = size(targetPack,1);
+    TargetNum(i) = filesNO;
+    for j = 1:filesNO
+        IMsingle = imread(targetPack(j).name);
+        imwrite(IMsingle,[TargetFolder,'.tif'],'WriteMode','append');
+    end
+end
+disp('Exporting finished!');
+%% Motion_correction
+if ~exist('TargetList')
+    cd(mainPath);
+    load('TargetList.mat');
+end
+SettingsMatName = 'C:\Users\linji\Desktop\EZcalcium\ez_settings.mat'; % the location of ez_settings.mat
+load(SettingsMatName);
+
+j = 0;
+for i = 1:len
+    if(~isempty(strfind(TargetList{i},ch2Lable)))
+        continue;
+    end
+    j = j + 1;
+    TargetFiles{j,1} = [TargetList{i},'.tif'];
+end
+motcor_settings.to_process_list = TargetFiles;
+motcor_settings.has_channel2 = hasCh2;
+save(SettingsMatName, 'motcor_settings');
+cd('C:\Users\linji\Desktop\EZcalcium'); % the loacation of EZcalcium
+Mfiledir = pwd;
+packDir = fileparts(Mfiledir);
+addpath(genpath(Mfiledir));%add path for searching functions
+% cd([Mfiledir,'\EZcalcium-master']);
+disp('Motion orrection...');
+% ezcalcium;
+ez_motion_correction;
+beep;
+pause(1);
+beep;
+
+% for i = 1:len
+%     TargetFiles{i,1} = [TargetList{i},'.tif'];
+%     delete([TargetFolder,'.tif']);
+% end
+%%   =======================================================================================
+function TargetList = scanDir(CurrentPath,TargetLabel,TargetType)
+files = dir(CurrentPath);
+len = length(files);
+TargetList = {};
+index = 1;
+for i = 1 : len
+    if (strcmp(files(i).name, '.') == 1) ...
+            || (strcmp(files(i).name, '..') == 1)
+        continue;
+    end
+    if files(i).isdir == 1
+        CurrentPath1 = [CurrentPath, '\', files(i).name];
+        disp(CurrentPath1);
+        tmpName = scanDir(CurrentPath1,TargetLabel,TargetType);
+        for j = 1 : length(tmpName)
+            TargetList{index} = tmpName{j};
+            index = index + 1;
+        end
+    end
+    switch TargetType
+        case 1
+            if files(i).isdir == 1 && ~isempty(strfind(files(i).name,TargetLabel))
+                TargetList{index} = fullfile(CurrentPath, '\', files(i).name);
+                index = index + 1;
+            end
+        case 2
+            if files(i).isdir == 0 && ~isempty(strfind(files(i).name,TargetLabel))
+                TargetList{index} = fullfile(CurrentPath, '\', files(i).name);
+                index = index + 1;
+            end
+        case 3
+            if ~isempty(strfind(files(i).name,TargetLabel))
+                TargetList{index} = fullfile(CurrentPath, '\', files(i).name);
+                index = index + 1;
+            end
+    end 
+end
+end
+
+
+function[dessert] = confirm_follow_process(question)
+answer = questdlg(question, 'confirm dialog', 'Yes', 'No', 'Yes');
+
+% Handle response
+dessert = 1;
+switch answer
+    case 'Yes'
+        dessert = true;
+    case 'No'
+        dessert = false;
+end
+end
---
+++ README.md
@@ -0,0 +1,29 @@
+# EZcalcium
+Calcium imaging analysis made easy. EZcalcium is a flexible, user-friendly toolbox for analysis of calcium imaging data, controlled by a set of intuitive graphical user interfaces (GUI) based on MATLAB.
+
+## Components & general workflow
+
+EZcalcium contains three main modules: **Motion Correction**, **ROI Detection**, and **ROI Refinement**. Typically, an imaging data file is processed through this workflow.
+
+**Motion Correction**, based on the [NoRMCorre toolbox](https://github.com/porteralab/NoRMCorre), applies image alignment to correct for motion artifacts in the raw imaging data.
+
+**ROI Detection**, based on the [CaImAn toolbox](https://github.com/porteralab/CaImAn-MATLAB), performs automated ROI detection, signal extraction, and deconvolution of fluorescence calcium signals.
+
+**ROI Refinement** enables the user to inspect deteced ROIs, manually exclude ROIs, and use automated, customized ROI exclusion criteria, including spatial and activity-dependent metrics.
+
+Step-by-step instructions for using each modules can be found on the [EZcalcium GitHub Wiki page](https://github.com/porteralab/EZcalcium/wiki), as well as the included [HELP.pdf](https://github.com/porteralab/EZcalcium/blob/master/HELP.pdf) file.
+
+## System requirements and installation
+
+The amount of required available system memory depends on the size of the data being processed. Ideally, the amount of total system memory should be at least 3x of the size of a single raw, uncompressed data file. CPU requirements are minimal, but processing speed is vastly improved with multiple cores. The toolbox also runs faster when the data to be analyzed is stored on a solid-state drive, since large amounts of data must be read and, in the case of Motion Correction, written.
+
+We recommend 64-bit MATLAB R2020b (version 9.9) or newer on Windows or macOS for using the EZcalcium. The following MATLAB toolboxes are required:
+
+* Image Processing (required by **Motion Correction**, **ROI Detection** and **ROI Refinement**)
+* Parallel Computing (required by **Motion Correction** and **ROI Detection**)
+* Signal Processing (required by **ROI Detection**)
+* Statistics and Machine Learning (required by **Motion Correction**, **ROI Detection** and **ROI Refinement**)
+
+To install EZcalcium, go to the GitHub [releases](https://github.com/porteralab/EZcalcium/releases) page, download `EZcalcium.zip` of the latest release and extract the downloaded ZIP file to your place of choice. And then in MATLAB, go to HOME - ENVIRONMENT - Set Path, and in the Set Path window, click "Add with Subfolders...", select the folder that contains the files you just extracted, then click "Save" and "Close".
+
+Or if you are comfortable with Git, you can just clone this repository, as well as [CaImAn](https://github.com/porteralab/CaImAn-MATLAB) and [NoRMCorre](https://github.com/porteralab/NoRMCorre). Add the folders to MATLAB Path and you are good to go.
